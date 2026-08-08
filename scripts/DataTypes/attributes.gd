f681c618a6961d348237b169d21e2ae1925bd549
Slurp <sherwoodwt@gmail.com>
2026-08-08 13:42:23 -0700

    asdf

--- scripts/DataTypes/attributes.gd
+++ scripts/DataTypes/attributes.gd
@@ -10,10 +10,10 @@ var vector: Vector4i
 
 static func make_comperable(other: Attributes) -> Attributes:
 	var att = Attributes.new()
-	att.reliability = clampi(randi_range(other.reliability - 2, other.reliability + 1), 0, 5)
-	att.sociability = clampi(randi_range(other.sociability - 2, other.sociability + 1), 0, 5)
-	att.competence = clampi(randi_range(other.competence - 2, other.competence + 1), 0, 5)
-	att.technical = clampi(randi_range(other.technical - 2, other.technical + 1), 0, 5)
+	att.reliability = clampi(randi_range(other.reliability - 1, other.reliability + 1), 0, 5)
+	att.sociability = clampi(randi_range(other.sociability - 1, other.sociability + 1), 0, 5)
+	att.competence = clampi(randi_range(other.competence - 1, other.competence + 1), 0, 5)
+	att.technical = clampi(randi_range(other.technical - 1, other.technical + 1), 0, 5)
 	att.vector = Vector4i(att.reliability, att.sociability, att.competence, att.technical)
 	return att
 
