ad272dcaf09de2d72bf8af942e39a243900574ed
Slurp <sherwoodwt@gmail.com>
2026-08-10 18:36:44 -0700

    right/double click

--- scripts/portrait_button.gd
+++ scripts/portrait_button.gd
@@ -1,12 +1,11 @@
 class_name PortraitButton
 extends Button
 
+const HOLD_TIME: int = 60
+
 @onready var portrait: PortraitDisplay = $MarginContainer/Portrait
 
 var role: Role
-
-func _ready():
-	pressed.connect(handle_pressed)
 
 # used to modify existing button
 func set_role(role: Role):
@@ -14,6 +13,13 @@ func set_role(role: Role):
 	if role.employee:
 		portrait.set_employee(role.employee)
 
-func handle_pressed():
-	#if 
-	SignalBus.highlight.emit(self)
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var e = event as InputEventMouseButton
+		if e.is_released():
+			if e.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+				SignalBus.highlight.emit(self)
+			elif e.button_index == MouseButton.MOUSE_BUTTON_RIGHT:
+				SignalBus.focus_changed.emit(role)
+		if e.double_click and e.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+			SignalBus.focus_changed.emit(role)
